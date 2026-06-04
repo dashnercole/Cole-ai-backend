95af189feeb122c41c84b5a7160b9abdc2d590c3
Cole Dashner <dashnercole@gmail.com>
2026-06-04 10:47:09 -0400

    Update model version in ask.js

--- api/ask.js
+++ api/ask.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model: "llama-3.1-8b-instant",
+        model: "llama-3.3-70b-versatile",
         messages: [
           {
             role: "system",
